be790886125cc449a7aead0acf44fa1343170c2a
Daniel <vukyanh001@gmail.com>
2024-01-16 23:51:25 +0700

    add auth api

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.SERVER_PORT || 8080;
 connectDB();
 app.use(express.json());
 app.use("/tasks/", require("./routes/task"));
+app.use("/users/", require("./routes/task"));
 app.use(errorHandler)
 
 app.listen(port, () => {
